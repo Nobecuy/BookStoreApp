e93598e43604b115de25a1c8f233bd0055eaefb9
Nobe Ananda <anobe32@gmail.com>
2025-01-26 00:51:11 +0700

    Add project files.

---
+++ BookStore8/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore8.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Made Call To Weather Point");
+            try
+            {
+
+                throw new Exception("This is a our test exception");
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex,"An error occurred while getting the weather forecast");
+                throw;
+            }
+
+        }
+    }
+}
