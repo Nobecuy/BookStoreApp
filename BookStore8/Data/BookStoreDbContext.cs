f80368c3b92d12e6531c0664990da1b9728ee280
Nobe Ananda <anobe32@gmail.com>
2025-01-26 10:24:54 +0700

    scaffold sama Ef Core

---
+++ BookStore8/Data/BookStoreDbContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore8.Data;
+
+public partial class BookStoreDbContext : DbContext
+{
+    public BookStoreDbContext()
+    {
+    }
+
+    public BookStoreDbContext(DbContextOptions<BookStoreDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Author> Authors { get; set; }
+
+    public virtual DbSet<Book> Books { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Authors__3214EC07FA0D9AAF");
+
+            entity.Property(e => e.Bio).HasMaxLength(250);
+            entity.Property(e => e.FirstName).HasMaxLength(50);
+            entity.Property(e => e.LastName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Books__3214EC072B73B4D1");
+
+            entity.Property(e => e.Image).HasMaxLength(100);
+            entity.Property(e => e.Isbn)
+                .HasMaxLength(20)
+                .HasColumnName("ISBN");
+            entity.Property(e => e.Price).HasColumnType("decimal(18, 2)");
+            entity.Property(e => e.Summary).HasMaxLength(250);
+            entity.Property(e => e.Title).HasMaxLength(100);
+
+            entity.HasOne(d => d.Author).WithMany(p => p.Books)
+                .HasForeignKey(d => d.AuthorId)
+                .HasConstraintName("FK_Books_ToTable");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
